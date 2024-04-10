ea9dd9da88b48cfd36db3ae393b26b1b3a128ac4
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2024-04-10 19:26:08 +0800

    Add indicator for previous page

--- app/src/main/java/com/kaithebuilder/sturrel/ui/components/ToolbarView.kt
+++ app/src/main/java/com/kaithebuilder/sturrel/ui/components/ToolbarView.kt
@@ -2,16 +2,23 @@ package com.kaithebuilder.sturrel.ui.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.kaithebuilder.sturrel.model.sturrelVocab.FoldersDataManager
+import java.util.UUID
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -24,19 +31,38 @@ fun ToolbarView(
     Scaffold(
         topBar = {
             Column {
-                TopAppBar(
+                CenterAlignedTopAppBar(
                     title = {
                         Text(text = title)
                     },
                     navigationIcon = {
                         if (nav.currentDestination?.route != "root") {
-                            IconButton(onClick = {
-                                nav.popBackStack()
-                            }) {
-                                Icon(
-                                    imageVector = Icons.Filled.ArrowBack,
-                                    contentDescription = "Localized description"
-                                )
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                IconButton(onClick = {
+                                    nav.popBackStack()
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.ArrowBack,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                                if (nav.previousBackStackEntry?.destination?.route == "root") {
+                                    Text("Folders")
+                                } else {
+                                    val name = nav.previousBackStackEntry?.arguments?.getString("folderId")
+                                    if (name != null) {
+                                        Text(
+                                            text = FoldersDataManager.instance.getFolder(
+                                                UUID.fromString(name)
+                                            )!!.name,
+                                            modifier = Modifier
+                                                .width(80.dp),
+                                            maxLines = 2
+                                        )
+                                    }
+                                }
                             }
                         }
                     },

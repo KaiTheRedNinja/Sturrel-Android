11da829973ef1d8ebd4b78bf7b6355c10f838055
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2024-04-10 10:25:31 +0800

    Implement pretty UI for vocab detail view

---
+++ app/src/main/java/com/kaithebuilder/sturrel/NavList.kt
@@ -0,0 +1,82 @@
+package com.kaithebuilder.sturrel
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+@Composable
+fun NavList(title: String, nav: NavHostController, view: LazyListScope.() -> Unit) {
+    ToolbarView(title = title, nav = nav) {
+        LazyColumn(
+            Modifier
+                .fillMaxWidth()
+                .padding(all = 10.dp)
+                .padding(horizontal = 10.dp),
+            content = view
+        )
+    }
+}
+
+@Composable
+fun ListSectionHeader(header: String) {
+    Text(
+        text = header,
+        modifier = Modifier.padding(start = 15.dp, top = 10.dp),
+        color = Color.Gray
+    )
+}
+
+@Composable
+fun ListItem(
+    index: Int,
+    totalSize: Int,
+    view: @Composable () -> Unit
+) {
+    var cardMod = Modifier
+        .padding(bottom = if (index == totalSize - 1) 5.dp else 0.dp)
+        .fillMaxWidth()
+    val cornerRadius = 10.dp
+
+    cardMod = if (totalSize == 1) {
+        cardMod.padding(vertical = 5.dp)
+    } else when (index) {
+        0 -> cardMod.padding(top = 5.dp)
+        totalSize - 1 -> cardMod.padding(bottom = 10.dp)
+        else -> cardMod.padding(vertical = 0.dp)
+    }
+
+    ElevatedCard(
+        modifier = cardMod,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        elevation = CardDefaults.cardElevation(1.dp),
+        shape = when (totalSize) {
+            1 -> RoundedCornerShape(cornerRadius)
+            else -> when (index) {
+                0 -> RoundedCornerShape(topStart = cornerRadius, topEnd = cornerRadius)
+                totalSize - 1 -> RoundedCornerShape(bottomStart = cornerRadius, bottomEnd = cornerRadius)
+                else -> RoundedCornerShape(0)
+            }
+        }
+    ) {
+        view()
+        if (index+1 != totalSize) {
+            Divider(
+                color = Color.Gray.copy(alpha = 0.2f)
+            )
+        }
+    }
+}
--- app/src/main/java/com/kaithebuilder/sturrel/VocabDetailView.kt
+++ app/src/main/java/com/kaithebuilder/sturrel/VocabDetailView.kt
@@ -4,19 +4,28 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -30,6 +39,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.kaithebuilder.sturrel.model.pinYin.PinYin
@@ -54,59 +64,32 @@ private fun VocabDetailViewContents(
     vocab: Vocab,
     nav: NavHostController
 ) {
-    ToolbarView(title = vocab.word, nav = nav) {
-        LazyColumn(
-            Modifier
-                .fillMaxWidth()
-                .padding(all = 10.dp)
-        ) {
-            item {
-                Column {
-                    Text(
-                        text = "Definition",
-                        modifier = Modifier.padding(start = 15.dp, top = 30.dp),
-                        color = Color.Gray
-                    )
-                    if (vocab.englishDefinition.isNotEmpty()) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(15.dp)
-                        ) {
-                            Text(
-                                text = vocab.englishDefinition,
-                                modifier = Modifier.weight(1f)
-                            )
-                        }
-                        Divider()
-                    }
-                    if (vocab.definition.isNotEmpty()) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(15.dp)
-                        ) {
-                            Text(
-                                text = vocab.definition,
-                                modifier = Modifier.weight(1f)
-                            )
-                        }
-                        Divider()
-                    }
-                }
+    NavList(title = "Vocab", nav = nav) {
+        item {
+            if (vocab.isHCL) {
+                Spacer(modifier = Modifier
+                    .padding(top = 50.dp))
+                ListSectionHeader(header = "Higher Chinese")
             }
+            WordSection(word = vocab.word)
+        }
 
-            if (vocab.sentences.isNotEmpty()) {
-                item {
-                    Text(
-                        text = "Sentences",
-                        modifier = Modifier.padding(start = 15.dp, top = 30.dp),
-                        color = Color.Gray
-                    )
-                }
-                items(vocab.sentences) { sentence ->
+        if (vocab.englishDefinition.isNotEmpty() || vocab.definition.isNotEmpty()) {
+            item {
+                ListSectionHeader(header = "Definition")
+                DefinitionSection(
+                    english = vocab.englishDefinition,
+                    chinese = vocab.definition
+                )
+            }
+        }
+
+        if (vocab.sentences.isNotEmpty()) {
+            item {
+                ListSectionHeader(header = "Sentences")
+            }
+            itemsIndexed(vocab.sentences) { index, sentence ->
+                ListItem(index = index, totalSize = vocab.sentences.count()) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
                         modifier = Modifier
@@ -118,19 +101,16 @@ private fun VocabDetailViewContents(
                             modifier = Modifier.weight(1f)
                         )
                     }
-                    Divider()
                 }
             }
+        }
 
-            if (vocab.wordBuilding.isNotEmpty()) {
-                item {
-                    Text(
-                        text = "Words",
-                        modifier = Modifier.padding(start = 15.dp, top = 30.dp),
-                        color = Color.Gray
-                    )
-                }
-                items(vocab.wordBuilding) { word ->
+        if (vocab.wordBuilding.isNotEmpty()) {
+            item {
+                ListSectionHeader(header = "Words")
+            }
+            itemsIndexed(vocab.wordBuilding) { index, word ->
+                ListItem(index = index, totalSize = vocab.wordBuilding.count()) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
                         modifier = Modifier
@@ -142,8 +122,66 @@ private fun VocabDetailViewContents(
                             modifier = Modifier.weight(1f)
                         )
                     }
-                    Divider()
                 }
+            }
+        }
+    }
+}
+
+@Composable
+private fun WordSection(word: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = word,
+            style = TextStyle(
+                fontSize = 36.sp, // Adjust the font size as needed
+                color = Color.Black, // Customize the color
+                fontWeight = FontWeight.Bold
+                // You can customize other properties such as font family, letter spacing, etc. here
+            ),
+            modifier = Modifier
+                .padding(vertical = 16.dp) // Adjust the padding as needed
+        )
+        Text(
+            text = PinYin.instance.getPinyinString(word)
+        )
+    }
+}
+
+@Composable
+private fun DefinitionSection(english: String, chinese: String) {
+    val count = english.isNotEmpty().compareTo(false) + chinese.isNotEmpty().compareTo(false)
+    if (english.isNotEmpty()) {
+        ListItem(index = 0, totalSize = count) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp)
+            ) {
+                Text(
+                    text = english,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+    }
+    if (chinese.isNotEmpty()) {
+        ListItem(index = if (english.isEmpty()) 0 else 1, totalSize = count) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp)
+            ) {
+                Text(
+                    text = chinese,
+                    modifier = Modifier.weight(1f)
+                )
             }
         }
     }
@@ -155,7 +193,7 @@ private fun VocabDetailViewPreviews() {
     VocabDetailViewContents(
         vocab = Vocab(
             word = "世界",
-            isHCL = false,
+            isHCL = true,
             englishDefinition = "World; Earth",
             definition = "地球",
             sentences = listOf("你好世界", "世界很大"),
